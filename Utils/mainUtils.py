2695793a4f6b4803e0db3d6a0146879c7dc224d3
es2y <vyn.es2y@gmail.com>
2026-03-08 00:36:57 +0100

    add internal logging

--- Utils/mainUtils.py
+++ Utils/mainUtils.py
@@ -3,5 +3,7 @@ from discord.ext import commands
 from discord import app_commands, ui
 import asyncio
 import json
+from Utils.config import *
+import time
 
 bot = commands.Bot(command_prefix="?", help_command=None, intents=discord.Intents.all(), chunk_guilds_at_startup=False)
--- Utils/utilsLoader.py
+++ Utils/utilsLoader.py
@@ -1,1 +1,3 @@
 from Utils.mainUtils import *
+
+from Functions.Events.on_guild_join_leave import on_guild_join as on_guild_join_func, on_guild_remove as on_guild_remove_func
