18ae1078c54b12c69fdc6d53d2d6219fdbafa983
es2y <vyn.es2y@gmail.com>
2026-03-08 20:30:00 +0100

    add auto default config

--- Functions/Events/on_guild_join_leave.py
+++ Functions/Events/on_guild_join_leave.py
@@ -4,11 +4,17 @@ from Utils.mainUtils import *
 def on_guild_join(guild: discord.Guild):
     errors = []
     try:
-        # Function from Utils/config.py, will add an entry of guild.id to the Databases/modlog.json file (if there is no entry yet)
+        # Function from Utils/config.py, will add an entry of guild.id to the Databases/modlog.json file (if there is no entry yet).
         initModLog(guild.id)
     except Exception as e:
         # Add Exception to errors in case something goes wrong.
         errors.append(f"initModLog: {e}")
+    try:
+        # Function from Utils/config.py, will set the configuration of guild.id to default (if there is no entry yet).
+        initConfig(guild.id)
+    except Exception as e:
+        # Same as above.
+        errors.append(f"initConfig: {e}")
     # Create log to save in internal logs.
     log = {
         "type": "joinGuild",
@@ -21,7 +27,7 @@ def on_guild_join(guild: discord.Guild):
 
 def on_guild_remove(guild: discord.Guild):
     # Works just like on_guild_join (without the initModLog function).
-    # Modlogs will not be deleted upon bot removal/rejoin. initModLog ignores guilds with previous logs.
+    # Modlogs and Config will not be deleted upon bot removal/rejoin. initModLog and initConfig ignore guilds with previous logs/configs.
     errors = []
     log = {
         "type": "leaveGuild",
